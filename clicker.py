c7a0a8e192e4d54c36736130eeba2c1980c8c52f
vwnx1 <vwnx123@gmail.com>
2022-08-17 15:44:47 -0300

    .

--- clicker.py
+++ clicker.py
@@ -3,22 +3,6 @@ from keyboard import is_pressed
 #1366x768 -> x=679 y=533
 
 #Main click
-'''
-def accept_match(x, y):
-    while True:
-        click(x=679, y=533)
-        if is_pressed('q'):
-            break
-'''
-def string_separator(string, n):
-    x = string.find(' ')
-    str_1 = string[0:x]
-    str_2 = string[-x:]
-    if n == 1:
-        return str_1
-    else:
-        return str_2
-
 def accept_match(rx, ry):
     x=679
     y=533
@@ -40,3 +24,14 @@ def accept_match(rx, ry):
         click(x,y)
         if is_pressed('q'):
             break
+
+
+
+def string_separator(string, n):
+    x = string.find(' ')
+    str_1 = string[0:x]
+    str_2 = string[-x:]
+    if n == 1:
+        return str_1
+    else:
+        return str_2
