c3a4577794940cc12cc04a83daf98bfae37be8a0
vwnx1 <vwnx123@gmail.com>
2022-08-17 08:38:05 -0300

    Criados testes para o status da página

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from django.test import Client
+
+class StatusCodeTest(TestCase):
+    def test_home_status_code(self):
+        c = Client()
+        resp = c.get('')
+        self.assertEqual(resp.status_code, 200, 'Erro ao acessar a página')
+    def test_clicker_status_code(self):
+        c = Client()
+        resp = c.get('/clicker')
+        self.assertEqual(resp.status_code, 200, 'Erro ao acessar a página')
+    def test_check_data_status_code(self):
+        c = Client()
+        resp = c.get('/check_values')
+        self.assertEqual(resp.status_code, 200, 'Erro ao acessar a página')
