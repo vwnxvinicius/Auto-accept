8594d546ec5bd5b3a01b07d13000ea6fb0df5e28
vwnx1 <vwnx123@gmail.com>
2022-08-17 15:14:50 -0300

    Removida a url da view check_values

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,8 @@
-from turtle import home
+
 from django.urls import path
-from .views import home, clicker #check_values
+from main import views
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('clicker', clicker, name='clicker'),
-    #path('check_values', check_values, name='check_values')
+    path('', views.home, name='home'),
+    path('clicker', views.clicker, name='clicker')
 ]
